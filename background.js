a91e2a162623808ad246105bc596f5b7d8f2dd5b
Mert Can USLU <whoave@Mert-MacBook-Air.local>
2022-04-28 21:43:14 +0300

    badge

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.action.onClicked.addListener(async () => {
     const enabled = !(await chrome.storage.local.get('enabled')).enabled;
     const path = `images/${enabled ? 'on' : 'off'}.png`;
+    chrome.action.setBadgeText ( { text: enabled ? 'A' : 'K' } );
+    chrome.action.setBadgeBackgroundColor({color: "orange"});
+
     chrome.action.setIcon({
         path
     });
@@ -17,6 +20,9 @@ chrome.tabs.onUpdated.addListener(async (tabId, change, tab) => {
             enabled
         });
     }
+    chrome.action.setBadgeText ( { text: enabled ? 'A' : 'K' } );
+    chrome.action.setBadgeBackgroundColor({color: "orange"});
+
     const engelList = "-site:msn.com -site:internethaber.com -site:fotomac.com.tr -site:tgrthaber.com.tr -site:aksam.com.tr -site:trthaber.com -site:star.com.tr -site:ahaber.com.tr -site:sporx.com -site:gazetevatan.com -site:sozcu.com.tr -site:haberturk.com -site:mynet.com -site:acunn.com -site:haber7.com -site:sabah.com.tr -site:yenisafak.com -site:hurriyet.com.tr -site:cnnturk.com -site:youtube.com -site:pinterest.com -site:milliyet.com.tr -site:haberler.com -site:posta.com.tr -site:fanatik.com.tr -site:cumhuriyet.com.tr -site:takvim.com.tr -site:ensonhaber.com -site:sondakika.com -site:t24.com.tr -site:ntv.com.tr";
     const engellenen = encodeURI(engelList);
 
